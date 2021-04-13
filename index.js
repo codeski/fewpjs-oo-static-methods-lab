0756164523bdd3c0e49d012382612388cb38f492
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-04-12 19:50:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(sentence){
+    let splitSentence = sentence.split(" ")
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    splitSentence[0] = Formatter.capitalize(splitSentence[0])
+    for(let i = 1; i < splitSentence.length; i++){
+      if(exceptions.find(word => word === splitSentence[i])){
+      } else { splitSentence[i] = Formatter.capitalize(splitSentence[i])}
+    }
+
+    return splitSentence.join(' ')
+  }
 }
